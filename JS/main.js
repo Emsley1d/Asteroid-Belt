182a67f3ab98fd57bc93b03cc2cd0f09e7120aba
emsleyd <emsleyd@gmail.com>
2022-08-24 13:35:54 +0100

    Wednesday's progress thus far

--- JS/main.js
+++ JS/main.js
@@ -1,192 +1,157 @@
-const grid = document.querySelector('.grid')
-const resultsDisplay = document.querySelector('.results')
-let currentPlayerIndex = 202
-let currentStationIndex = 219
-let width = 15
-let direction = 1
-let cometId
-let goingRight = true
-let cometsRemoved = []
-let results = 0
+const grid = document.querySelector(".grid");
+const resultsDisplay = document.querySelector(".results");
+let currentPlayerIndex = 318;
+let currentStationIndex = 285;
+let width = 22;
+let direction = 1;
+let cometId;
+let goingRight = true;
+let cometsRemoved = [];
+let results = 0;
 
-for (let i = 0; i < 225; i++) {
-  const square = document.createElement('div')
-  grid.appendChild(square)
+for (let i = 0; i < 320; i++) {
+  const square = document.createElement("div");
+  grid.appendChild(square);
 }
 
-const squares = Array.from(document.querySelectorAll('.grid div'))
+const squares = Array.from(document.querySelectorAll(".grid div"));
 
 //Starting Locations
 const approachingComets = [
-  0,1,2,3,4,5,6,7,8,9,
-  15,16,17,18,19,20,21,22,23,24,
-  30,31,32,33,34,35,36,37,38,39
-]
+    0, 1, 2, 7, 8, 9, 10, 13, 14, 17,
+    23, 24, 26, 27, 29, 30, 31, 32, 34, 35, 39, 40, 41,
+    44, 46, 47, 48, 49, 52, 53, 57, 58, 59, 60, 62, 63,
+    66, 69, 70, 75, 76, 77, 80, 82, 83, 85,
+    88, 90, 93, 94, 96, 98, 99, 100,
+  ];
 
-const movingStation = [219, 220, 221]
 
-squares[currentStationIndex].classList.add('station')
+const Station = [219, 220, 221];
 
+squares[currentStationIndex].classList.add("station");
 
 function draw() {
   for (let i = 0; i < approachingComets.length; i++) {
-    if(!cometsRemoved.includes(i)) {
-      squares[approachingComets[i]].classList.add('comet')
+    if (!cometsRemoved.includes(i)) {
+      squares[approachingComets[i]].classList.add("comet");
     }
   }
 }
 
-draw()
+draw();
 
 function remove() {
   for (let i = 0; i < approachingComets.length; i++) {
-    squares[approachingComets[i]].classList.remove('comet')
+    squares[approachingComets[i]].classList.remove("comet");
   }
 }
 
-squares[currentPlayerIndex].classList.add('player')
-
+squares[currentPlayerIndex].classList.add("player");
 
 function movePlayer(e) {
-  squares[currentPlayerIndex].classList.remove('player')
-  switch(e.key){
-    case 'ArrowLeft':
-      if (currentPlayerIndex % width !== 0) currentPlayerIndex -=1
-      break
-    case 'ArrowRight' :
-      if (currentPlayerIndex % width < width -1) currentPlayerIndex +=1
-      break
+  squares[currentPlayerIndex].classList.remove("player");
+  switch (e.key) {
+    case "ArrowLeft":
+      if (currentPlayerIndex % width !== 0) currentPlayerIndex -= 1;
+      break;
+    case "ArrowRight":
+      if (currentPlayerIndex % width < width - 1) currentPlayerIndex += 1;
+      break;
   }
-  squares[currentPlayerIndex].classList.add('player')
+  squares[currentPlayerIndex].classList.add("player");
 }
-document.addEventListener('keydown', movePlayer)
+document.addEventListener("keydown", movePlayer);
 
 function moveComets() {
-  const leftEdge = approachingComets[0] % width === 0
-  const rightEdge = approachingComets[approachingComets.length - 1] % width === width -1
-  remove()
+  const leftEdge = approachingComets[0] % width === 0;
+  const rightEdge =
+    approachingComets[approachingComets.length - 2] % width === width - 1;
+  remove();
 
   //Identifies the board edges
   if (rightEdge && goingRight) {
     for (let i = 0; i < approachingComets.length; i++) {
-      approachingComets[i] += width +1
-      direction = -1
-      goingRight = false
+      approachingComets[i] += width + 1;
+      direction = -1;
+      goingRight = false;
     }
   }
 
-  if(leftEdge && !goingRight) {
+  if (leftEdge && !goingRight) {
     for (let i = 0; i < approachingComets.length; i++) {
-      approachingComets[i] += width -1
-      direction = 1
-      goingRight = true
+      approachingComets[i] += width - 1;
+      direction = 1;
+      goingRight = true;
     }
   }
 
   for (let i = 0; i < approachingComets.length; i++) {
-    approachingComets[i] += direction
+    approachingComets[i] += direction;
   }
 
-  draw()
+  draw();
 
-  if (squares[currentPlayerIndex].classList.contains('comet', 'player')) {
-    resultsDisplay.innerHTML = 'GAME OVER'
-    clearInterval(cometId)
+  if (squares[currentPlayerIndex].classList.contains("comet", "player")) {
+    resultsDisplay.innerHTML = "GAME OVER";
+    clearInterval(cometId);
   }
 
   for (let i = 0; i < approachingComets.length; i++) {
-    if(approachingComets[i] > (squares.length)) {
-      resultsDisplay.innerHTML = 'GAME OVER'
-      clearInterval(cometId)
+    if (approachingComets[i] > squares.length) {
+      resultsDisplay.innerHTML = "GAME OVER";
+      clearInterval(cometId);
     }
   }
   if (cometsRemoved.length === approachingComets.length) {
-    resultsDisplay.innerHTML = 'YOU WIN'
-    clearInterval(cometId)
+    resultsDisplay.innerHTML = "YOU WIN";
+    clearInterval(cometId);
   }
 }
 
-// **********************************************************
-
-function moveStation() {
-    const leftEdge = movingStation[0] % width === 0
-    const rightEdge = movingStation[movingStation.length - 1] % width === width -1
-    remove()
-  
-    //Identifies the board edges
-    if (rightEdge && goingRight) {
-      for (let i = 0; i < movingStation.length; i++) {
-        movingStation[i] += width +1
-        direction = -1
-        goingRight = false
-      }
-    }
-  
-    if(leftEdge && !goingRight) {
-      for (let i = 0; i < movingStation.length; i++) {
-        movingStation[i] += width -1
-        direction = 1
-        goingRight = true
-      }
-    }
-  
-    for (let i = 0; i < movingStation.length; i++) {
-        movingStation[i] += direction
-    }
-  
-    draw()
-  
-    // if (squares[currentPlayerIndex].classList.contains('comet', 'player')) {
-    //   resultsDisplay.innerHTML = 'GAME OVER'
-    //   clearInterval(cometId)
-    // }
-  
-    // for (let i = 0; i < approachingComets.length; i++) {
-    //   if(approachingComets[i] > (squares.length)) {
-    //     resultsDisplay.innerHTML = 'GAME OVER'
-    //     clearInterval(cometId)
-    //   }
-    // }
-    // if (cometsRemoved.length === approachingComets.length) {
-    //   resultsDisplay.innerHTML = 'YOU WIN'
-    //   clearInterval(cometId)
-    // }
-  }
-
-// **************************************************************
-
-cometId = setInterval(moveComets, 500)
-
+cometId = setInterval(moveComets, 200);
 
 function shoot(e) {
-  let missileId
-  let currentLaserIndex = currentPlayerIndex
+  let missileId;
+  let currentMissileIndex = currentPlayerIndex;
   function moveMissile() {
-    squares[currentLaserIndex].classList.remove('missile')
-    currentLaserIndex -= width
-    squares[currentLaserIndex].classList.add('missile')
+    squares[currentMissileIndex].classList.remove("missile");
+    currentMissileIndex -= width;
+    squares[currentMissileIndex].classList.add("missile");
 
-    if (squares[currentLaserIndex].classList.contains('comet')) {
-      squares[currentLaserIndex].classList.remove('missile')
-      squares[currentLaserIndex].classList.remove('comet')
-      squares[currentLaserIndex].classList.add('explosion')
+    if (squares[currentMissileIndex].classList.contains("comet")) {
+      squares[currentMissileIndex].classList.remove("missile");
+      squares[currentMissileIndex].classList.remove("comet");
+      squares[currentMissileIndex].classList.add("explosion");
 
-      setTimeout(()=> squares[currentLaserIndex].classList.remove('explosion'), 50)
-      clearInterval(missileId)
+      setTimeout(
+        () => squares[currentMissileIndex].classList.remove("explosion"),
+        60
+      );
+      clearInterval(missileId);
 
-      const cometRemoved = approachingComets.indexOf(currentLaserIndex)
-      cometsRemoved.push(cometRemoved)
-      results++
-      resultsDisplay.innerHTML = results
-      console.log(cometsRemoved)      
+    if (squares[currentStationIndex].classList.contains("comet")) {
+        squares[currentMissileIndex].classList.remove("comet");
+        squares[currentMissileIndex].classList.remove("station");
+        squares[currentMissileIndex].classList.add("explosion");
 
-    }             
+      setTimeout(
+        () => squares[currentMissileIndex].classList.remove("explosion"),
+        60
+      );
+      clearInterval(missileId);
 
+      const cometRemoved = approachingComets.indexOf(currentMissileIndex);
+      cometsRemoved.push(cometRemoved);
+      results++;
+      resultsDisplay.innerHTML = results;
+      console.log(cometsRemoved);
+    }
   }
-  switch(e.key) {
-    case 'ArrowUp':
-      missileId = setInterval(moveMissile, 100)
+  }
+  switch (e.key) {
+    case "ArrowUp":
+      missileId = setInterval(moveMissile, 400);
   }
 }
 
-document.addEventListener('keydown', shoot)
+document.addEventListener("keydown", shoot);
